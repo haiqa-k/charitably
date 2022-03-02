2d300099bd3230402a2c42407aadd515e120ff9e
haiqa-k <79232961+haiqa-k@users.noreply.github.com>
2022-03-02 03:24:20 -0500

    demo.js works
    Map works with multiple markers and info bubbles

--- static/js/demo.js
+++ static/js/demo.js
@@ -11,11 +11,24 @@
   group.addObject(marker);
 }
 
+function getLat(){
+  var json = eventList;
+  var obj = JSON.parse(json[0]);
+  return parseFloat(obj["latitude"])
+}
+function getLong(){
+  var json = eventList;
+  var obj = JSON.parse(json[0]);
+  return parseFloat(obj["longitude"])
+}
+
+
 /**
  * Add markers
  * Clicking on a marker opens an infobubble which holds HTML content related to the marker.
  * @param {H.Map} map A HERE Map instance within the application
  */
+
 function addInfoBubble(map) {
   var group = new H.map.Group();
 //*********
@@ -25,8 +38,8 @@ function addInfoBubble(map) {
   var json = eventList;
   console.log("this is json: ",json)
   console.log("this is json: ",json.length)
-  var lat_constant = 0;
-  var long_constant = 0;
+
+  list = []
   for(var i = 0; i < json.length; i++) {
     var result = json[i];
     var obj = JSON.parse(result);
@@ -34,17 +47,46 @@ function addInfoBubble(map) {
     var url = "https://www.google.com/search?q="+name.replace(" ", "");
     var description = obj["description"];
     var latitude = obj["latitude"]
+    latitude_constant = obj["latitude"]
     var longitude = obj["longitude"];
+    long_constant = obj["longitude"];
     var start_date = obj["start_date"];
-    lat_constant = obj["latitude"]
-    long_constant = obj["longitude"]
+    // var obj = [name, url, description, latitude, longitude, start_date]
+    var obj = [name, parseFloat(latitude), parseFloat(longitude), url]
+    list[i] = obj
 
-    addMarkerToGroup(group, {lat: latitude, lng: longitude},
-    '<div><a href="'+url+'">'+name+'</a></div>' +
-    '<div>'+description+'<br /><strong>Date:</strong> '+start_date+'</div>');
+
+  }
+   console.log("type of list: ", typeof list)
+    //const coords = [["The Inaugural Scholarship Sneaker Ball Fundraiser", 35.22337329999999, -80.8482202], ["Big South Tourney Takeover: SkoolDAZE", 35.22764529999998, -80.8391473]];
+    // const coords = [
+    //     [48.8,2.35, "hi"],
+    //     [41.9,12.5, "hi"],
+    //     [52.5,13.38, "hi"]
+    // ];
+    list.forEach((el)=> {
+        lat_ = parseFloat(el[1])
+        long_ = parseFloat(el[2])
+          addMarkerToGroup(group, {lat: lat_, lng: long_},
+    '<div><a href="'+el[3]+'">'+el[0]+'</a></div>');
     map.addObject(group);
-    
-    group.addEventListener('tap', function (evt) {
+    });
+    console.log("type of coords: ", typeof coords)
+    // const coords = [
+    // ["The Inaugural Scholarship Sneaker Ball Fundraiser", 35.22337329999999, -80.8482202], 
+    // ["Big South Tourney Takeover: SkoolDAZE", 35.22764529999998, -80.8391473]
+    // ];
+    // 
+
+    // coords.forEach((el)=> {
+    //     lat_ = parseFloat(el[1])
+    //     long_ = parseFloat(el[2])
+    //       addMarkerToGroup(group, {lat: lat_, lng: long_},
+    // '<div><a>'+el[0]+'</a></div>');
+    // map.addObject(group);
+    // });
+
+    group.addEventListener('tap', function (evt) { 
     // event target is the marker itself, group is a parent event target
     // for all objects that it contains
     var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
@@ -53,8 +95,8 @@ function addInfoBubble(map) {
     });
     // show info bubble
     ui.addBubble(bubble);
-  }, false);
-  }
+    }, false);
+    
 
 
 
@@ -105,11 +147,12 @@ var platform = new H.service.Platform({
 });
 var defaultLayers = platform.createDefaultLayers();
 
+
 // initialize a map - this map is centered over 2930 Chestnut Street, Philadelphia
 var map = new H.Map(document.getElementById('map'),
   defaultLayers.vector.normal.map, {
-  center: {lat: 39.952670, lng: -75.183190},
-  zoom: 0,
+  center: {lat: getLat(), lng: getLong()},
+  zoom: 8,
   pixelRatio: window.devicePixelRatio || 1
 });
 
